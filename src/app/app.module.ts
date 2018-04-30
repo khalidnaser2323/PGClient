0da376251044d36890cff5601d128f22de052b5b
khalidnaser2323 <khalid.naser2323@gmail.com>
2018-04-30 15:34:34 +0200

    First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomePageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
