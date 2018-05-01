b65243cb4474dc2e6e735cc38a505a78086cc165
Nihal <33658943+NihalElhaggar@users.noreply.github.com>
2018-05-01 12:08:04 +0200

    hooked with product Model

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {ProductModel} from '../../Models/ProductModel'
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
+  Products:ProductModel[]=
+  [
+  new ProductModel('child.html','img/img1.png','Leadership Pillar','Et suscipit menandri est, ut mea iriure imperdiet, at his docendi.'),
+  new ProductModel('child.html','img/img2.png','Leadership Pillar','Et suscipit menandri est, ut mea iriure imperdiet, at his docendi.'),
+  new ProductModel('child.html','img/img3.png','Leadership Pillar','Et suscipit menandri est, ut mea iriure imperdiet, at his docendi.'),
+  new ProductModel('child.html','img/img1.png','Leadership Pillar','Et suscipit menandri est, ut mea iriure imperdiet, at his docendi.'),
+  new ProductModel('child.html','img/img2.png','Leadership Pillar','Et suscipit menandri est, ut mea iriure imperdiet, at his docendi.'),
+  new ProductModel('child.html','img/img3.png','Leadership Pillar','Et suscipit menandri est, ut mea iriure imperdiet, at his docendi.'),
+  new ProductModel('child.html','img/img1.png','Leadership Pillar','Et suscipit menandri est, ut mea iriure imperdiet, at his docendi.'),
+  new ProductModel('child.html','img/img2.png','Leadership Pillar','Et suscipit menandri est, ut mea iriure imperdiet, at his docendi.'),
+  new ProductModel('child.html','img/img3.png','Leadership Pillar','Et suscipit menandri est, ut mea iriure imperdiet, at his docendi.'),
+  new ProductModel('child.html','img/img1.png','Leadership Pillar','Et suscipit menandri est, ut mea iriure imperdiet, at his docendi.')
+];
   constructor() { }
 
   ngOnInit() {
+
   }
 
 }
