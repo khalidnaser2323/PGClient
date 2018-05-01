a3651091b11532274cbdbf75cce361eef926f517
Nihal <33658943+NihalElhaggar@users.noreply.github.com>
2018-05-01 12:01:01 +0200

    Add files via upload

---
+++ src/app/Models/ProductModel.ts
@@ -0,0 +1,12 @@
+export class ProductModel
+{
+    constructor(
+        public ChildUrl:string,
+        public ProuductImg:string,
+        public name:string,
+        public Discribtion:String, 
+    )
+ {
+
+ }  
+}
