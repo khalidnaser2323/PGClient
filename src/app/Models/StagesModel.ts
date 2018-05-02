113b5c5cfcbc4172dd76bbbec4b54cd73a2732eb
Nihal <nihal.elhaggar@gmail.com>
2018-05-02 11:27:49 +0200

    dynamicStageTemplate

---
+++ src/app/Models/StagesModel.ts
@@ -0,0 +1,13 @@
+export class StageModel
+{
+    constructor(
+        public Price:string,
+        public currency:string,
+        public StageImg:string,   
+        public StageDiscribtion:String, 
+    
+    )
+ {
+
+ }  
+}
--- src/app/pages/stages-template/stages-template.component.ts
+++ src/app/pages/stages-template/stages-template.component.ts
@@ -1,11 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import { StageModel} from '../../Models/StagesModel'
 @Component({
   selector: 'app-stages-template',
   templateUrl: './stages-template.component.html',
   styleUrls: ['./stages-template.component.css']
 })
 export class StagesTemplateComponent implements OnInit {
+
+  Stages:StageModel[]=
+  [
+   new StageModel('120,0000 ','EGP','img/stage1.png',' Make. Be. They are. Is abundantly earth. Replenish place appear so evening day seas set.'),
+   new StageModel(' 328,0000 ','EGP','img/stage2.png',' Make. Be. They are. Is abundantly earth. Replenish place appear so evening day seas set.'), 
+   new StageModel('510,0000','EGP','img/stage3.png',' Make. Be. They are. Is abundantly earth. Replenish place appear so evening day seas set.'),
+   new StageModel('964,0000','EGP','img/stage4.png',' Make. Be. They are. Is abundantly earth. Replenish place appear so evening day seas set.') 
+  ]
+
 
   constructor() { }
 
