f0bd3576f88bc88cdc4dc09d8dbb701f5d3ef67a
khalidnaser2323 <khalid.naser2323@gmail.com>
2018-06-11 15:43:56 +0200

    [Technical] Fix temp11 UI

--- src/app/pages/template9/template9.component.ts
+++ src/app/pages/template9/template9.component.ts
@@ -1,12 +1,11 @@
-import { Component, OnInit,ViewChild,ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { Chart } from 'chart.js';
 
-interface template9
-{
-  percentageData:string[];
-  color:string[];
-  labels:string[];
-  title:string;
+interface template9 {
+  percentageData: string[];
+  color: string[];
+  labels: string[];
+  title: string;
 }
 @Component({
   selector: 'app-template9',
@@ -14,69 +13,69 @@ interface template9
   styleUrls: ['./template9.component.css']
 })
 export class Template9Component implements OnInit {
-  ctx:any;
-  chart=[];
-  chartTemp:template9;
+  ctx: any;
+  chart = [];
+  chartTemp: template9;
   @ViewChild('canvas') canvasRef: ElementRef;
   constructor() {
-    this.chartTemp=
-    {
-       percentageData:['40','20','30'] ,
-        color:['rgb(51, 153, 255)','rgb(214, 214, 194)','rgb(124, 124, 80)'],
-        labels:['leaderShip','PSRH','HSE'],
-        title:'Custom Chart Title',
-      
-    }
-   }
+    this.chartTemp =
+      {
+        percentageData: ['40', '20', '30'],
+        color: null,
+        labels: ['leaderShip', 'PSRH', 'HSE'],
+        title: 'Custom Chart Title',
+
+      }
+  }
 
   ngOnInit() {
     this.ctx = this.canvasRef.nativeElement.getContext('2d');
     this.chart = new Chart(this.ctx, {
       type: 'pie',
       data: {
-       
-     datasets: [{
-          data:this.chartTemp.percentageData,
-          backgroundColor:this.chartTemp.color,
-          borderWidth:'7',     
-      }],
 
-      labels:this.chartTemp.labels, 
-      
-  },
-  options: {
-    title: {
-      display: true,
-      text: this.chartTemp.title,
-      fontSize:20,
-  },
-  
-    responsive:true,
-    responsiveAnimationDuration:5,
-    
-    legend : {
-      display:'true',
-          position:'bottom',
-          
+        datasets: [{
+          data: this.chartTemp.percentageData,
+          backgroundColor: this.chartTemp.color ? this.chartTemp.color : ['red', 'blue', 'yellow', 'orange', 'grey', 'pink', 'chocolate', 'lime', 'khaki', 'purple'],
+          borderWidth: '7',
+        }],
+
+        labels: this.chartTemp.labels,
+
+      },
+      options: {
+        title: {
+          display: true,
+          text: this.chartTemp.title,
+          fontSize: 20,
+        },
+
+        responsive: true,
+        responsiveAnimationDuration: 5,
+
+        legend: {
+          display: 'true',
+          position: 'bottom',
+
           labels: {
-            boxWidth:70,
-            fontSize:20,
-            fontColor:"rgb(0, 0, 0)",
-            tooltips :{
+            boxWidth: 70,
+            fontSize: 20,
+            fontColor: "rgb(0, 0, 0)",
+            tooltips: {
               callbacks: {
-                labelColor:' rgb(71, 90, 255)',
-                
+                labelColor: ' rgb(71, 90, 255)',
+
               },
-             
-                },
-                },
-               
-              }
-              },
-           
-     
-  });
-}
+
+            },
+          },
+
+        }
+      },
+
+
+    });
+  }
 }
 
 
--- src/app/pages/templete11/templete11.component.ts
+++ src/app/pages/templete11/templete11.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-templete11',
   templateUrl: './templete11.component.html',
   styleUrls: ['./templete11.component.css']
 })
 export class Templete11Component implements OnInit {
-
+  activePanel: string = "1";
   constructor() { }
 
   ngOnInit() {
+  }
+  showPanel(id: string) {
+    console.log("id to show:  " + id);
+    this.activePanel = id;
   }
 
 }
