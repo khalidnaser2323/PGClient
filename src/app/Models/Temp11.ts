6b6dfcc15040f42459136b63dcee2fbad4ea05a3
khalidnaser2323 <khalid.naser2323@gmail.com>
2018-06-12 15:26:19 +0200

    [Technical] Template 11 implementation

---
+++ src/app/Models/Temp11.ts
@@ -0,0 +1,17 @@
+interface Temp11 {
+    colOneHeader: string,
+    colOneText: string,
+    colTwoHeader: string,
+    colTwoText: string,
+    colThreeHeader: string,
+    colThreeText: string,
+    colFourText: string,
+    colFiveText: string,
+    colSixText: string,
+    colSevenText: string,
+    colEightText: string,
+    colNineText: string,
+    colTenText: string,
+    colElevenText: string,
+    colTwelveText: string
+}
--- src/app/pages/templete11/templete11.component.ts
+++ src/app/pages/templete11/templete11.component.ts
@@ -8,7 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Templete11Component implements OnInit {
   activePanel: string = "1";
-  constructor() { }
+  temp11: Temp11;
+  constructor() {
+    this.temp11 = {
+      colEightText: "text8",
+      colElevenText: "text11",
+      colFiveText: "tex5",
+      colFourText: "text4",
+      colNineText: "text9",
+      colOneHeader: "header",
+      colOneText: "text 1",
+      colSevenText: "text7",
+      colSixText: "text6",
+      colTenText: "text10",
+      colThreeHeader: "header3",
+      colThreeText: "text3",
+      colTwelveText: "text12",
+      colTwoHeader: "header 2",
+      colTwoText: "text 2"
+    }
+  }
 
   ngOnInit() {
   }
